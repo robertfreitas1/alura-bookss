a5f5f5d43e5a40cc1e2887a3d664969c11f4b292
robertfreitas1 <168023572+robertfreitas1@users.noreply.github.com>
2025-03-16 14:57:45 -0300

    adicionando logo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import logo from './images/logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Alura
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='app-header'>
+        <div className='logo'>
+        <img src={logo} alt="Logo da Alura"/>
+        <p> <strong>Alura </strong> Books</p>
+
+        </div>
+        
       </header>
     </div>
   );
