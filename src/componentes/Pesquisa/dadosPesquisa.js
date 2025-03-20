8790e8a014873f581226f0554d77cb8c868696ac
robertfreitas1 <168023572+robertfreitas1@users.noreply.github.com>
2025-03-20 15:50:13 -0300

    Manipulando pesquisas

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,12 @@
+import livro from '../../images/livro.png'
+import livro2 from '../../images/livro2.png'
+
+export const livros = [
+    {nome: 'Liderança em Design', id: 1, src: livro},
+    {nome: 'JavaScript Assertivo', id: 2, src:livro},
+    {nome: 'Cangaceiro em JavaScript', id: 3, src:livro},
+    {nome: 'Aventureiros do Java', id: 4, src:livro},
+]
+
+
+
--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,58 +1,81 @@
-import Input from '../Input';
-import styled from 'styled-components';
-import React, { useState } from "react";
+import Input from '../Input'
+import styled from 'styled-components'
+import { useState } from 'react'
+import { livros } from './dadosPesquisa'
 
 const PesquisaContainer = styled.section`
-   background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
-   color: #FFF;
-   text-align: center;
-   padding: 85px 0;
-   height: 270px;
-   width: 100%;
+    background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
+    color: #FFF;
+    text-align: center;
+    padding: 85px 0;
+    height: 270px;
+    width: 100%;
 `;
 
 const Titulo = styled.h2`
-   color: #FFF;
-   font-size: 36px;
-   text-align: center;
-   width: 100%;
+    color: #FFF;
+    font-size: 36px;
+    text-align: center;
+    width: 100%;
 `;
 
 const Subtitulo = styled.h3`
-   font-size: 16px;
-   font-weight: 500;
-   margin-bottom: 40px;
+    font-size: 16px;
+    font-weight: 500;
+    margin-bottom: 40px;
+`;
+
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+    p {
+        width: 200px;
+    }
+    img {
+        width: 100px;
+    }
+    &:hover {
+        border: 1px solid white;
+    }
 `;
 
 function Pesquisa() {
-  const [textoDigitado, setTextoDigitado] = useState(''); // Estado inicial
-
-  const livros = ["React", "JavaScript", "Node.js", "Python", "CSS", "HTML"];
-
-  // Filtra os livros com base na pesquisa
-  const livrosFiltrados = livros.filter((livro) =>
-    livro.toLowerCase().includes(textoDigitado.toLowerCase())
-  );
+  const [livrosPesquisados, setLivrosPesquisados] = useState([]);
 
   return (
-    <PesquisaContainer>   
-      <Titulo>Já sabe por onde começar?</Titulo>
-      <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
-      
-      <Input 
-        placeholder="Qual vai ser a sua próxima leitura?"
-        onChange={evento => setTextoDigitado(evento.target.value)}
-      />
+      <PesquisaContainer>
+          <Titulo>Já sabe por onde começar?</Titulo>
+          <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
+          <Input
+              placeholder="Escreva sua próxima leitura"
+              onChange={evento => {
+                  const textoDigitado = evento.target.value.toLowerCase();
 
-      {/* Só mostra a lista se houver texto digitado */}
-      {textoDigitado && (
-        <ul>
-          {livrosFiltrados.map((livro, index) => (
-            <li key={index}>{livro}</li>
+                  // Se o campo estiver vazio, reseta a lista de livros pesquisados
+                  if (textoDigitado === '') {
+                      setLivrosPesquisados([]); 
+                  } else {
+                      const resultadoPesquisa = livros.filter(livro =>
+                          livro.nome.toLowerCase().includes(textoDigitado)
+                      );
+                      setLivrosPesquisados(resultadoPesquisa);
+                  }
+              }}
+          />
+
+          {/* Renderiza os livros pesquisados, ou uma mensagem se não houver livros */}
+          {livrosPesquisados.length === 0 && <p></p>}
+
+          {livrosPesquisados.map(livro => (
+              <Resultado key={livro.id}>
+                  <img src={livro.src} alt={livro.nome} />
+                  <p>{livro.nome}</p>
+              </Resultado>
           ))}
-        </ul>
-      )}
-    </PesquisaContainer>  
+      </PesquisaContainer>
   );
 }
 
