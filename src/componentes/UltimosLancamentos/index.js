19c7b1af462b1559854877c0b5777befe3a7fd4a
robertfreitas1 <168023572+robertfreitas1@users.noreply.github.com>
2025-03-20 22:24:54 -0300

    Usando props

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,5 +1,7 @@
 import { livros } from "./dadosUltimosLancamentos";
 import {Titulo} from '../Titulo'
+import CardRecomenda from "../CardRecomenda";
+import imagemLivro from '../../images/livro2.png'
 import styled from 'styled-components'
 
 
@@ -34,6 +36,13 @@ function UltimosLancamentos (){
                 <img src={livro.src}></img>
             ))}
             </NovosLivrosContainer> 
+          <CardRecomenda
+             titulo="Talvez você se interesse por..."
+             subtitulo="Angular 11"
+             descricao="Contruindo uma aplicação com a plataforma google"
+             img={imagemLivro}
+          />
+
         </UltimosLancamentosContainer>
     )
 }
